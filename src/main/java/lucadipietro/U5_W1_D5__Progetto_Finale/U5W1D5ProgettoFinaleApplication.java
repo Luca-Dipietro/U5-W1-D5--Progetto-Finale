c10d0a631c9478ad9182940d6730e21db99a2e61
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-06-21 10:23:32 +0200

    Start Project

---
+++ src/main/java/lucadipietro/U5_W1_D5__Progetto_Finale/U5W1D5ProgettoFinaleApplication.java
@@ -0,0 +1,13 @@
+package lucadipietro.U5_W1_D5__Progetto_Finale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D5ProgettoFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W1D5ProgettoFinaleApplication.class, args);
+	}
+
+}
